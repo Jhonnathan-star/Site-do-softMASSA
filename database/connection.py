ebc834a10ef7eabcabd5756935c55abeb8645755
Jhonnathan <jhon250603@gmail.com>
2025-06-05 10:09:41 -0300

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -1,14 +1,19 @@
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis do .env
+load_dotenv()
 
 def conectar():
     try:
         conn = mysql.connector.connect(
-            host='turntable.proxy.rlwy.net',
-            port=43507,
-            user='root',
-            password='xxxxxxxxx',
-            database='railway'
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
         if conn.is_connected():
             print("Conexão bem-sucedida ao banco de dados")
@@ -16,3 +21,4 @@ def conectar():
     except Error as e:
         print(f"Erro ao conectar: {e}")
         return None
+
