ef44ff882fd4fa59c7379bcc65f11ad58914de92
Jhonnathan <jhon250603@gmail.com>
2025-06-06 21:06:57 -0300

    Update app.py
    nome do menu alterado

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.success(f"Bem-vindo, {st.session_state.usuario}!")
 
 # Menu lateral
 opcoes = [
-    "Menu Principal",
+    "Home",
     "Inserir dados de telas",
     "Inserir somente horários",
     "Criar predição da semana",
@@ -73,7 +73,7 @@ def executar_pagina(funcao):
         st.session_state.pagina = "Menu Principal"
         st.rerun()
 
-if st.session_state.pagina == "Menu Principal":
+if st.session_state.pagina == "Home":
     st.info("Use o menu lateral para navegar pelas funcionalidades.")
 
     # Botão de logout no menu principal
